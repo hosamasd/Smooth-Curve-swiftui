40cc467dc642819437999283288c329316098f65
hosam <hosamelmalt@gmail.com>
2020-12-09 15:12:05 +0200

    Initial Commit

---
+++ Smooth Curve/Smooth_CurveApp.swift
@@ -0,0 +1,17 @@
+//
+//  Smooth_CurveApp.swift
+//  Smooth Curve
+//
+//  Created by hosam on 12/9/20.
+//
+
+import SwiftUI
+
+@main
+struct Smooth_CurveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
